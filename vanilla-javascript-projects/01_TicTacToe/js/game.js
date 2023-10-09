85ba766198abdd3b9faa3cc75d2e04d6e7ea2169
ashikmoosa <ashikmoosa96@gmail.com>
2023-10-09 15:58:20 +0530

    Removed unwanted asset, fixed grid style, added mouseover

--- vanilla-javascript-projects/01_TicTacToe/js/game.js
+++ vanilla-javascript-projects/01_TicTacToe/js/game.js
@@ -1,11 +1,40 @@
 let starPlaced = false;
 let planetPlaced = false;
-let firstTurn = "star";
+let currentTurn = "star";
+let hoverElem;
 
 // References to DOM Elements
 const gameFramer = document.querySelector(".game-framer");
 
-// Events
+// Events - Interactions
+// Hover Event - onMouseOver
+gameFramer.addEventListener("mouseover", (event) => {
+  // debugger;
+  console.log("Hovered", event);
+  hoverElem = event.target;
+  if (event.target.classList.contains("item-slot")) {
+    if (currentTurn === "star") {
+      event.target.parentElement.children[1].hidden = false;
+      event.target.parentElement.children[2].hidden = true;
+    } else {
+      event.target.parentElement.children[1].hidden = true;
+      event.target.parentElement.children[2].hidden = false;
+    }
+  }
+});
+
+// Hover Event - onMouseLeave
+gameFramer.addEventListener("mouseout", (event) => {
+  // Check if the clicked element has the class "item" or any other relevant class
+  // debugger;
+  console.log("Hovered", event);
+  if (event.target.classList.contains("item-slot")) {
+    event.target.parentElement.children[1].hidden = true;
+    event.target.parentElement.children[2].hidden = true;
+  }
+});
+
+// Click Event - Placing Objects
 gameFramer.addEventListener("click", (event) => {
   // Check if the clicked element has the class "item" or any other relevant class
   console.log("TestClick", event);
